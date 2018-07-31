25dcd5071ddeb8b304312bf5a70a326d65b58d5b
wolflex888 <wolflex888@gmail.com>
2018-07-31 21:21:37 +0800

    add another new line util

--- script/util.py
+++ script/util.py
@@ -3,6 +3,6 @@ def helloworld():
     print("hello world!")
     print("bye bye world!")
     print("add new line.")
-
+    print("add another new line.")
 if __name__ == "__main__":
     helloworld()
