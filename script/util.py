2b52a5e59088afca35bc29c1ed89f9208a782664
wolflex888 <wolflex888@gmail.com>
2018-08-01 09:36:59 +0800

    t

--- script/util.py
+++ script/util.py
@@ -1,13 +1,28 @@
 #!/Users/juichanglu/anaconda3/bin/python3
-def helloworld():
-    print("hello world!")
-    print("bye bye world!")
-    print("add new line.")
-    print("add another new line.")
-    print("add another new new line.")
-    print("add another new new new line.")
-    print("this is for test branch!!")
-    print("this is to test test branch")
-    print("this tests test branch")
+import os
+import random
+def quote_process(path_raw, path_write):
+    
+    file = open(path_raw, "r")
+    write = open(path_write, "w")
+    i = 0
+    for line in file:
+        if line != "\n":
+            line = line.rstrip()
+            write.write(line)
+        else:
+            write.write("\t")
+            i+=1
+    file.close()
+    write.close()
+    return i
+def print_random_quote(path_read, quote_numb):
+    data_f = open(path_read, "r")
+    rand = random.randint(0, quote_numb-1)
+    for line in data_f:
+        quotes = line.split("\t")
+    print(quotes[rand])
 if __name__ == "__main__":
-    helloworld()
+    quote_numb = quote_process("../data/quotes.txt", "../data/quotes_ready.tab")
+    print_random_quote("../data/quotes_ready.tab", quote_numb)
+
