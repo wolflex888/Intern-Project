cd831cc8cdc75c843905b3c50b25c791efdc43b2
wolflex888 <wolflex888@gmail.com>
2018-08-01 09:46:11 +0800

    quotation test

--- script/util.py
+++ script/util.py
@@ -5,24 +5,22 @@ def quote_process(path_raw, path_write):
     
     file = open(path_raw, "r")
     write = open(path_write, "w")
-    i = 0
     for line in file:
         if line != "\n":
             line = line.rstrip()
             write.write(line)
         else:
             write.write("\t")
-            i+=1
     file.close()
     write.close()
-    return i
-def print_random_quote(path_read, quote_numb):
+def print_random_quote(path_read):
     data_f = open(path_read, "r")
-    rand = random.randint(0, quote_numb-1)
+    
     for line in data_f:
         quotes = line.split("\t")
+    rand = random.randint(0, len(quotes)-1)
     print(quotes[rand])
 if __name__ == "__main__":
-    quote_numb = quote_process("../data/quotes.txt", "../data/quotes_ready.tab")
-    print_random_quote("../data/quotes_ready.tab", quote_numb)
+    quote_process("data/quotes.txt", "data/quotes_ready.tab")
+    print_random_quote("data/quotes_ready.tab")
 
